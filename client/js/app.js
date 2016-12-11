c1ebc878a8205c54becf2c51976bdb8a0eab77f1
Erik Watson <championchap@gmail.com>
2016-12-11 20:54:07 +0000

    Added a link to the source code in the developers console

--- client/js/app.js
+++ client/js/app.js
@@ -1,12 +1,14 @@
 import Events from './events'
 
 function displayGreeting () {
+  const github = 'https://github.com/championchap/ErikWatson.me'
   const messages = [
     'Hello, nosey.',
     "There isn't much to see in here, really.",
     'This site was made with Express, Pug, SASS, ES6 and Gulp.',
     "It really didn't need to be.",
-    "It's a very simple site based on the lovely Base16 colour scheme."
+    "It's a very simple site based on the lovely Base16 colour scheme.",
+    `Source code for this site available at: ${github}`
   ]
 
   messages.forEach(message => {
